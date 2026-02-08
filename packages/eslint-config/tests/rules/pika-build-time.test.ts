65c0d5cf992b956903a05536d31d5da46e516044
DevilTea <ch870814@gmail.com>
2026-02-08 19:53:35 +0800

    test(11-01): verify pika-build-time rule logic
    - Add RuleTester setup for pika-build-time
    - Test valid cases: static strings, objects, arrays, templates, numbers
    - Test invalid cases: identifiers, function calls, computed properties, spread elements

---
+++ packages/eslint-config/tests/rules/pika-build-time.test.ts
@@ -0,0 +1,63 @@
+import { RuleTester } from '@typescript-eslint/rule-tester'
+import { afterAll, describe, it } from 'vitest'
+import { pikaBuildTime } from '../../src/rules/pika-build-time'
+
+RuleTester.afterAll = afterAll
+RuleTester.describe = describe
+RuleTester.it = it
+
+const ruleTester = new RuleTester({
+	languageOptions: {
+		parserOptions: {
+			ecmaVersion: 2020,
+			sourceType: 'module',
+		},
+	},
+})
+
+ruleTester.run('pika-build-time', pikaBuildTime, {
+	valid: [
+		'pika({ color: \'red\' })',
+		'pika.str(\'foo\')',
+		'pika([\'a\', \'b\'])',
+		'pika({ nested: { val: 1 } })',
+		'pika(`val-static`)',
+		'pika(-1)',
+		'otherCall(someVar)',
+	],
+	invalid: [
+		{
+			code: 'pika(someVar)',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika({ color: someVar })',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika(\'a\' + \'b\')',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			// eslint-disable-next-line no-template-curly-in-string
+			code: 'pika(`val-${x}`)',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika(condition ? \'a\' : \'b\')',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika(...items)',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika({ ...obj })',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+		{
+			code: 'pika.str(someVar)',
+			errors: [{ messageId: 'dynamicArgument' }],
+		},
+	],
+})
