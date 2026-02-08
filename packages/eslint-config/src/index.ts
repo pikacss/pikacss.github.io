029ce5ef04fe61f5c78565068ca84f5e87e41346
DevilTea <ch870814@gmail.com>
2026-02-08 19:53:01 +0800

    feat(11-01): export pika-build-time rule in plugin configuration
    - Replace default array export with plugin object
    - Export rules object

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,1 +1,13 @@
-export default []
+import { pikaBuildTime } from './rules/pika-build-time'
+
+const plugin = {
+	meta: {
+		name: '@pikacss/eslint-config',
+		version: '0.0.40',
+	},
+	rules: {
+		'pika-build-time': pikaBuildTime,
+	},
+}
+
+export default plugin
